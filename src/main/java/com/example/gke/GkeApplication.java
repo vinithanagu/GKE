04019c893d72650bf0f6e21593c982053f254e35
ragav25 <ragavravi25@gmail.com>
2021-03-15 22:35:27 +0000

    initial-commit

---
+++ src/main/java/com/example/gke/GkeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gke/GkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
